c74bfc96c82d249ee4655662296d9cf236814f2a
Nobu Nakata <nobu.nakata@gmail.com>
2026-07-10 21:32:21 +0900

    test: cover Linear and SRGB color spaces in GPUStatistics

--- Assets/Tests/Editor/TestGPUStatistics.cs
+++ Assets/Tests/Editor/TestGPUStatistics.cs
@@ -7,87 +7,104 @@ using UnityEngine;
 namespace GPUImageStatisticsSystem.Editor {
 
 	public class TestGPUStatistics {
-        public const string IMAGE_NAME = "SampleImage";
+		public const string IMAGE_NAME = "SampleImage";
 
-        public const string CS_NAME = "Statistics";
-        public const string KERN_SUM = "Sum";
+		public const string CS_NAME = "Statistics";
+		public const string KERN_SUM = "Sum";
 
-        public const string PROP_IMAGE = "Image";
-        public const string PROP_SIZE = "Size";
-        public const string PROP_TOTAL = "Total";
+		public const string PROP_IMAGE = "Image";
+		public const string PROP_SIZE = "Size";
+		public const string PROP_TOTAL = "Total";
 
-        public const int NUM_THREADS = 16;
-        public const float ACCURACY_RELATIVE = 1e-2f;
-        public const float ACCURACY_ABSOLUTE = 1e-6f;
+		public const int NUM_THREADS = 16;
+		public const float ACCURACY_RELATIVE = 1e-2f;
+		public const float ACCURACY_ABSOLUTE = 1e-6f;
 
-        [Test]
-        public void GPUStatistics() {
-            var tex = Resources.Load<Texture2D>(IMAGE_NAME);
-            var pixels = (PlayerSettings.colorSpace == ColorSpace.Linear)
-                ? tex.GetPixels().Select(c => c.linear).ColorToVector4()
-                : tex.GetPixels().ColorToVector4();
-            var expectedSum = pixels.Total();
-            var expectedAverage = pixels.Average();
-            var expectedCovariance = pixels.Covariance(expectedAverage);
+		[Test]
+		public void GPUStatisticsLinearColorSpace() {
+			RunGPUStatistics(StatisticsColorSpace.Linear);
+		}
 
-            var stat = new GPUStatistics();
-            var sum = stat.Sum(tex);
-            AreApproximatelyEqual(expectedSum, sum);
+		[Test]
+		public void GPUStatisticsSRGBColorSpace() {
+			RunGPUStatistics(StatisticsColorSpace.SRGB);
+		}
 
-            var average = stat.Average(tex);
-            AreApproximatelyEqual(expectedAverage, average);
+		void RunGPUStatistics(StatisticsColorSpace colorSpace) {
+			var tex = Resources.Load<Texture2D>(IMAGE_NAME);
+			var pixels = ExpectedPixels(tex, colorSpace);
+			var expectedSum = pixels.Total();
+			var expectedAverage = pixels.Average();
+			var expectedCovariance = pixels.Covariance(expectedAverage);
 
-            var covariance = stat.Covariance(tex, out average);
-            AreApproximatelyEqual(expectedAverage, average);
-            AreApproximatelyEqual(expectedCovariance, covariance);
-        }
+			var stat = new GPUStatistics { ColorSpace = colorSpace };
+			var sum = stat.Sum(tex);
+			AreApproximatelyEqual(expectedSum, sum);
 
-        [Test]
-        public void GPUReduction() { 
-            var redwidth = 4;
-            var redheight = 4;
-            var redinput = new Vector4[redwidth * redheight];
-            using (var redbuf = new DisposableBuffer(redinput.Length, Marshal.SizeOf(redinput[0]))) {
-                for (var x = 0; x < redwidth; x++) {
-                    for (var y = 0; y < redheight; y++) {
-                        var v = Vector4.zero;
-                        v[x] = (y <= x ? 1f : 0f);
-                        redinput[x + y * redwidth] = v;
-                    }
-                }
-                redbuf.Buffer.SetData(redinput);
+			var average = stat.Average(tex);
+			AreApproximatelyEqual(expectedAverage, average);
 
-                var red = new GPUReduction();
-                var accum = red.Accumulate4(redbuf, redwidth, redheight);
-                AreApproximatelyEqual(new Vector4(1, 2, 3, 4), accum);
-            }
-        }
+			var covariance = stat.Covariance(tex, out average);
+			AreApproximatelyEqual(expectedAverage, average);
+			AreApproximatelyEqual(expectedCovariance, covariance);
+		}
 
-        private string Format(Vector4 v, string accuracy = "e2") {
-            return string.Format(
-                string.Format("({{0:{0}}},{{1:{0}}},{{2:{0}}},{{3:{0}}})", accuracy),
-                v.x, v.y, v.z, v.w);
-        }
-        private void AreApproximatelyEqual(Vector4 expected, Vector4 actual, float delta = ACCURACY_ABSOLUTE) {
-            for (var i = 0; i < 4; i++) {
-                Assert.AreEqual(expected[i], actual[i], Accuracy(expected[i], delta),
-                    string.Format("Vector not equal at i={0} : a={1} b={2}", i, expected[i], actual[i]));
-            }
-        }
-        private void AreApproximatelyEqual(Matrix4x4 expected, Matrix4x4 actual, float delta = ACCURACY_ABSOLUTE) {
-            for (var x = 0; x < 4; x++) {
-                for (var y = 0; y < 4; y++) {
-                    var va = expected[x, y];
-                    var vb = actual[x, y];
-                    Assert.AreEqual(va, vb, Accuracy(va, delta),
-                        string.Format("Matrix not equal at ({0},{1}) : a={2} b={3}",
-                        x, y, va, vb));
-                }
-            }
-        }
-        private float Accuracy(float value, float delta) {
-            return Mathf.Max(value * ACCURACY_RELATIVE, ACCURACY_ABSOLUTE);
-        }
-    }
+		static System.Collections.Generic.IEnumerable<Vector4> ExpectedPixels(
+				Texture2D tex, StatisticsColorSpace colorSpace) {
+			var colors = tex.GetPixels();
+			if (PlayerSettings.colorSpace == ColorSpace.Linear) {
+				if (colorSpace == StatisticsColorSpace.SRGB)
+					return colors.Select(c => (Vector4)c.gamma);
+				return colors.Select(c => (Vector4)c);
+			}
+			return colors.ColorToVector4();
+		}
 
+		[Test]
+		public void GPUReduction() {
+			var redwidth = 4;
+			var redheight = 4;
+			var redinput = new Vector4[redwidth * redheight];
+			using (var redbuf = new DisposableBuffer(redinput.Length, Marshal.SizeOf(redinput[0]))) {
+				for (var x = 0; x < redwidth; x++) {
+					for (var y = 0; y < redheight; y++) {
+						var v = Vector4.zero;
+						v[x] = (y <= x ? 1f : 0f);
+						redinput[x + y * redwidth] = v;
+					}
+				}
+				redbuf.Buffer.SetData(redinput);
+
+				var red = new GPUReduction();
+				var accum = red.Accumulate4(redbuf, redwidth, redheight);
+				AreApproximatelyEqual(new Vector4(1, 2, 3, 4), accum);
+			}
+		}
+
+		private string Format(Vector4 v, string accuracy = "e2") {
+			return string.Format(
+				string.Format("({{0:{0}}},{{1:{0}}},{{2:{0}}},{{3:{0}}})", accuracy),
+				v.x, v.y, v.z, v.w);
+		}
+		private void AreApproximatelyEqual(Vector4 expected, Vector4 actual, float delta = ACCURACY_ABSOLUTE) {
+			for (var i = 0; i < 4; i++) {
+				Assert.AreEqual(expected[i], actual[i], Accuracy(expected[i], delta),
+					string.Format("Vector not equal at i={0} : a={1} b={2}", i, expected[i], actual[i]));
+			}
+		}
+		private void AreApproximatelyEqual(Matrix4x4 expected, Matrix4x4 actual, float delta = ACCURACY_ABSOLUTE) {
+			for (var x = 0; x < 4; x++) {
+				for (var y = 0; y < 4; y++) {
+					var va = expected[x, y];
+					var vb = actual[x, y];
+					Assert.AreEqual(va, vb, Accuracy(va, delta),
+						string.Format("Matrix not equal at ({0},{1}) : a={2} b={3}",
+						x, y, va, vb));
+				}
+			}
+		}
+		private float Accuracy(float value, float delta) {
+			return Mathf.Max(value * ACCURACY_RELATIVE, ACCURACY_ABSOLUTE);
+		}
+	}
 }
